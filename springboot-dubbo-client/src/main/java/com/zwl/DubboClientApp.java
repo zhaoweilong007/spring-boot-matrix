f87dcce43c46b101f8d70d49db4a8a49511daffb
赵伟龙 <374244818@qq.com>
2021-10-18 17:59:23 +0800

    feat(dubbo):增加dubbo模块

---
+++ springboot-dubbo-client/src/main/java/com/zwl/DubboClientApp.java
@@ -0,0 +1,16 @@
+package com.zwl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ZhaoWeiLong
+ * @since 2021/10/18
+ **/
+@SpringBootApplication
+public class DubboClientApp {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DubboClientApp.class, args);
+  }
+}
