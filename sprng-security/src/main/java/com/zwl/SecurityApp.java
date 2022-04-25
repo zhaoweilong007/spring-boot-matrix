5d738685d2655b72044c4c6046d305d0e8639052
zhaoweilong <zhaoweilong176@gmail.com>
2022-04-25 10:01:53 +0800

    feat(security):初始化spring security模块

---
+++ sprng-security/src/main/java/com/zwl/SecurityApp.java
@@ -0,0 +1,18 @@
+package com.zwl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 描述：
+ *
+ * @author zwl
+ * @since 2022/4/1 16:08
+ **/
+@SpringBootApplication
+public class SecurityApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApp.class,args);
+    }
+}
