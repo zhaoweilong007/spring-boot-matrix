a5ff6f51164290204044460770db4053256b05d6
zhaoweilong <zhaoweilong176@gmail.com>
2022-04-25 18:34:25 +0800

    feat(docker):新增docker jib构建模块

---
+++ springboot-docker/src/test/java/com/zwl/springbootdocker/SpringbootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zwl.springbootdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
