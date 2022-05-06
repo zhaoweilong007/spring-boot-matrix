eff181c2a53e8e145c2e6d3a5656f47c9467159d
zhaoweilong <zhaoweilong176@gmail.com>
2022-05-06 17:50:36 +0800

    :sparkles: feat(*):增加Prometheus集成

---
+++ prometheus/src/main/java/com/zwl/PrometheusApp.java
@@ -0,0 +1,17 @@
+package com.zwl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 描述：
+ *
+ * @author zwl
+ * @since ${DATE} ${TIME}
+ */
+@SpringBootApplication
+public class PrometheusApp {
+  public static void main(String[] args) {
+    SpringApplication.run(PrometheusApp.class, args);
+  }
+}
