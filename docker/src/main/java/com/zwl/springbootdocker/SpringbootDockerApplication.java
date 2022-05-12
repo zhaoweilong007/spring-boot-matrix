0346e5f99d3af4674c455dea8d24ec7fea16dbf2
zhaoweilong <zhaoweilong176@gmail.com>
2022-05-12 16:20:21 +0800

    :bug: 修改elk配置

--- docker/src/main/java/com/zwl/springbootdocker/SpringbootDockerApplication.java
+++ docker/src/main/java/com/zwl/springbootdocker/SpringbootDockerApplication.java
@@ -1,9 +1,11 @@
 package com.zwl.springbootdocker;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class SpringbootDockerApplication {
 
   public static void main(String[] args) {
