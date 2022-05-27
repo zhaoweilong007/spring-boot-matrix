3555f976d60b8cf84e3d6ccc1817d46462e28449
zhaoweilong <zhaoweilong176@gmail.com>
2022-05-27 10:00:19 +0800

    :zap: 增加magic-api演示示例

---
+++ magic-api-demo/src/main/java/com/zwl/MagicApp.java
@@ -0,0 +1,17 @@
+package com.zwl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 描述：
+ *
+ * @author zwl
+ * @since ${DATE} ${TIME}
+ */
+@SpringBootApplication
+public class MagicApp {
+  public static void main(String[] args) {
+    SpringApplication.run(MagicApp.class, args);
+  }
+}
