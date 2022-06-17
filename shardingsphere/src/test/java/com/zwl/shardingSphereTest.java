289d1ec0661b30accd99d0f576e827d9f83712c8
zhaoweilong <zhaoweilong176@gmail.com>
2022-06-17 09:41:21 +0800

    升级依赖及更新配置

--- shardingsphere/src/test/java/com/zwl/shardingSphereTest.java
+++ shardingsphere/src/test/java/com/zwl/shardingSphereTest.java
@@ -1,13 +1,15 @@
 package com.zwl;
 
 import cn.hutool.db.Db;
-import cn.hutool.db.Entity;
 import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.google.common.collect.Lists;
 import com.zwl.entity.Answer;
 import com.zwl.mapper.AnswerMapper;
 import com.zwl.service.AnswerService;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,8 +18,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.sql.SQLException;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
+import java.util.Random;
 
 /**
  * @author ZhaoWeiLong
@@ -28,37 +29,47 @@ import java.util.stream.Collectors;
 @Slf4j
 public class shardingSphereTest {
 
-  @Autowired AnswerMapper answerMapper;
+    @Autowired
+    AnswerMapper answerMapper;
 
-  @Autowired AnswerService answerService;
+    @Autowired
+    AnswerService answerService;
 
-  // todo 测试插入数据分库分表
-  @Test
-  public void test() throws SQLException {
-    List<Entity> answers = Db.use().findAll("answer");
-    int size = answers.size();
-    log.info("size:{}", size);
-    log.info("first row:{}", Optional.ofNullable(answers.get(0)).orElse(null));
+    private List<Answer> list = Lists.newArrayList();
 
-    List<Answer> list = JSON.parseArray(JSON.toJSONString(answers), Answer.class);
-    list.forEach(answer -> answer.setId(null));
+    @Before
+    public void init() throws SQLException {
+        list = JSON.parseArray(JSON.toJSONString(Db.use().findAll("answer")), Answer.class);
+        log.info("init answer list,size:{}", list.size());
+    }
 
-    boolean batch = answerService.saveBatch(list);
-    log.info("批量插入：{}", batch);
+    // todo 测试插入数据分库分表
+    @Test
+    public void testInsert() {
+        list.forEach(answer -> answer.setId(null));
+        boolean batch = answerService.saveBatch(list);
+        log.info("批量插入：{}", batch);
 
-    list.forEach(
-        answer ->
-            log.info(
-                "topicId:{} % 2 = {},answerId:{} % 2 = {}",
-                answer.getTopicId(),
-                answer.getTopicId() % 2,
-                answer.getAnswerId(),
-                answer.getAnswerId() % 2));
+        list.forEach(
+                answer ->
+                        log.info(
+                                "topicId:{} % 2 = {},answerId:{} % 2 = {}",
+                                answer.getTopicId(),
+                                answer.getTopicId() % 2,
+                                answer.getAnswerId(),
+                                answer.getAnswerId() % 2));
+    }
 
-    List<Answer> answerList =
-        answerService.list(Wrappers.<Answer>lambdaQuery().eq(Answer::getAnswerId, 920583702));
-
-    log.info(
-        "answerList:{}", answerList.stream().map(Answer::getAnswerId).collect(Collectors.toList()));
-  }
+    @Test
+    public void testSelect() {
+        Random random = new Random();
+        int index = random.nextInt(list.size() - 1);
+        Answer answer = list.get(index);
+        Answer result = answerService.getOne(Wrappers.<Answer>lambdaQuery()
+                .eq(Answer::getTopicId, answer.getTopicId())
+                .eq(Answer::getAnswerId, answer.getAnswerId())
+                .eq(Answer::getQuestionId, answer.getQuestionId()));
+        Assert.assertEquals(answer.getAnswerId(), result.getAnswerId());
+        log.info("查询结果：{}", result);
+    }
 }
